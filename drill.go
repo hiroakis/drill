a29a672a411cbdea616eccb1fd9b613e438e7c77
Hiroaki Sano <hiroaki.sano.9stories@gmail.com>
2016-02-05 15:03:28 +0900

    first commit

---
+++ drill.go
@@ -0,0 +1,36 @@
+package drill
+
+import (
+	"os"
+	"path/filepath"
+	"strings"
+)
+
+func Down(base string, depth int, abs bool, dir bool) ([]string, error) {
+
+	var files []string
+
+	filepath.Walk(base, func(path string, info os.FileInfo, err error) error {
+		if info == nil {
+			return err
+		}
+		if info.IsDir() && !dir {
+			return nil
+		}
+
+		rel, _ := filepath.Rel(base, path)
+		if len(strings.Split(rel, "/")) > depth {
+			return nil
+		}
+		if abs {
+			absPath, _ := filepath.Abs(filepath.Dir(path))
+			files = append(files, filepath.Join(absPath, info.Name()))
+		} else {
+			files = append(files, path)
+		}
+
+		return err
+	})
+
+	return files, nil
+}
